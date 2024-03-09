461b2b00413d4d6ca28c99d70a3a3d65e4af2f2b
aumnatthacha <aummumdaeng@gmail.com>
2024-03-09 14:15:13 +0700

    update

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const cartRouter = require("./routes/cart.routes");
 
-
 const swaggerDefinition = {
   openapi: "3.0.0",
   info: {
