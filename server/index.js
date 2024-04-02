e8a06bd277086c39248cd4a3baf30a3cc4db207c
aumnatthacha <aummumdaeng@gmail.com>
2024-04-02 10:21:50 +0700

    update_user

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const productRouter = require("./routes/product.routes");
 const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const cartRouter = require("./routes/cart.routes");
+const UserRouter = require("./routes/user.routes");
+const jwt = require("jsonwebtoken");
 
 const swaggerDefinition = {
   openapi: "3.0.0",
@@ -26,7 +28,7 @@ const swaggerDefinition = {
 
   externalDocs: {
     description: "Download Swagger.json",
-    url: "/swagger.json"
+    url: "/swagger.json",
   },
 
   servers: [
@@ -51,21 +53,73 @@ const app = express();
 const CLIENT_URL = process.env.CLIENT_URL;
 app.use(cors({ credentials: true, origin: CLIENT_URL }));
 app.use(express.json());
-app.get("/swagger.json", (req,res)=> {
-  res.header("Content-Type","application/json");
+app.get("/swagger.json", (req, res) => {
+  res.header("Content-Type", "application/json");
   res.send(swaggerSpec);
-})
+});
 
 //Databse Connection
 const MONGODB_URL = process.env.MONGODB_URL;
 mongoose.connect(MONGODB_URL);
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));app.get("/", (req, res) => {
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get("/", (req, res) => {
   res.send("<h1>This is a RESTful API for SE Shop</h1>");
 });
 
 //Add Router
 app.use("/products", productRouter);
 app.use("/carts", cartRouter);
+app.use("/users", UserRouter);
+
+/**
+ * @swagger
+ * tags:
+ *   name: Authentication
+ *   description: API endpoints for user authentication
+ *
+ * components:
+ *   schemas:
+ *     TokenResponse:
+ *       type: object
+ *       properties:
+ *         token:
+ *           type: string
+ *           description: JWT token for authentication
+ *
+ * /jwt:
+ *   post:
+ *     summary: Generate JWT token for authentication
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: User email
+ *               name:
+ *                 type: string
+ *                 description: User password
+ *     responses:
+ *       200:
+ *         description: Successfully generated JWT token
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/TokenResponse'
+ *       500:
+ *         description: Internal server error
+ */
+app.post("/jwt", async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.SECRET, { expiresIn: "1h" });
+  res.json({ token });
+});
 
 //Run Server
 const PORT = process.env.PORT;
