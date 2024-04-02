6125ebc414f9c01db8c250a4dc79a7a0d93bbf71
aumnatthacha <aummumdaeng@gmail.com>
2024-04-03 02:31:23 +0700

    update

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -7,17 +7,15 @@ import { AuthContext } from "../context/AuthProvider";
 import axios from "axios";
 
 const Card = ({ item }) => {
-  const { user , setReload } = useContext(AuthContext);
+  const { user, setReload } = useContext(AuthContext);
   const { _id, name, image, price, description } = item;
   const [isHeartFilled, setIsHeartFilled] = useState(false);
 
   const handleHeartClick = () => {
     setIsHeartFilled(!isHeartFilled);
   };
-  const addToCart = async () => {
-    console.log(item);
-    console.log(_id);
 
+  const addToCart = async () => {
     if (!user) {
       Swal.fire({
         icon: "info",
@@ -35,7 +33,6 @@ const Card = ({ item }) => {
         quantity: 1,
       };
 
-      console.log(cartObjects);
       try {
         const response = await axios.post(
           "http://localhost:5000/carts",
@@ -46,22 +43,24 @@ const Card = ({ item }) => {
         if (response.status === 200) {
           Swal.fire({
             icon: "success",
-            title: "Your add item to cart",
+            title: "Item Added to Cart",
             showConfirmButton: false,
             timer: 1500,
+          }).then(() => {
+            // You can optionally perform any action after the notification is closed
+            console.log("Notification closed");
           });
-          console.log("เพิ่มสินค้าลงในตะกร้าเรียบร้อย:", response.data);
         }
       } catch (error) {
         Swal.fire({
-          icon: "info",
-          title: "Error add carts",
-          showConfirmButton: false,
-          timer: 1500,
+          icon: "error",
+          title: "Error",
+          text: "Failed to add item to cart",
         });
       }
     }
   };
+
   return (
     <div className="card shadow-xl relative mr-5 md:my-5">
       <div
@@ -79,7 +78,9 @@ const Card = ({ item }) => {
           <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
         </svg>
       </div>
-      <Link>
+      <Link to="#">
+        {" "}
+        {/* Update with valid link */}
         <figure>
           <img
             src={image}
@@ -89,7 +90,9 @@ const Card = ({ item }) => {
         </figure>
       </Link>
       <div className="card-body">
-        <Link>
+        <Link to="#">
+          {" "}
+          {/* Update with valid link */}
           <h2 className="card-title">{name}</h2>
         </Link>
         <p>{description} </p>
