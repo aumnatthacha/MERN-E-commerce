b755ad31d93e0e75085f0ad9c4cefb7289553f6b
aumnatthacha <aummumdaeng@gmail.com>
2024-04-03 13:03:21 +0700

    update

--- server/models/Carts.model.js
+++ server/models/Carts.model.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
-const CartItemSchema = new Schema(
-  {
 
-    product_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    name: { type: String, required: true },
+//Schema
+const CartSchema = new Schema({
+    product_id: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
     email: { type: String, required: true },
-    price: { type: String, required: true },
+    price: { type: Number, required: true },
+    name: { type: String, required: true },
     image: { type: String, required: true },
     quantity: { type: Number, required: true },
-  },
-  { timestamps: true }
-);
-const CartModel = model("Carts", CartItemSchema);
+}, {
+    timestamps: true,
+});
+
+const CartModel = model("Carts", CartSchema);
 module.exports = CartModel;
